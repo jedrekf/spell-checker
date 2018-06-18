aa05665fda694129dd506ee70127218b26c2c27a
piotr <plubon1337@gmail.com>
2018-06-18 08:57:56 +0200

    {odzial testow na batche

--- main.py
+++ main.py
@@ -59,37 +59,39 @@ with open('sentences_korpus.txt') as f:
     y = f.readlines()
     y = [z.strip() for z in y]
     y = [''.join(ch for ch in z if (ch.isalnum()) or ch == ' ') for z in y]
-x = x[:300]
-y = y[:300]
 word_count = 0
 error_count = 0
 correction_count = 0
 detected_count = 0
 sp = Spellchecker()
-results = sp.transform(x)
-corrections = []
-for idx, instance in enumerate(results):
-    for error in instance.errors:
-        print(error.word)
-        for candidate in sorted(error.candidates, key=lambda z: z.score):
-            print(candidate.word, candidate.score, candidate.similarity, candidate.distance)
-    correct = y[idx].lower().split(' ')
-    corrected = instance.get_corrected_sentence().lower().split(' ')
-    wrong = x[idx].lower().split(' ')
-    for token in zip(correct, wrong, corrected, instance.is_error):
-        word_count = word_count + 1
-        if token[0] != token[1]:
-            error_count = error_count + 1
-            if token[0] == token[2]:
-                correction_count = correction_count + 1
-            if token[3]:
-                detected_count = detected_count + 1
-            corrections.append(token)
-print(correction_count)
-print(detected_count)
-print(error_count)
-print(word_count)
-with open('results.csv', 'w') as out:
-    csv_out = csv.writer(out)
-    for row in corrections:
-        csv_out.writerow(row)
+for i in range(0, len(x), 100):
+    try:
+        end_idxx = min(len(x), i+100)
+        results = sp.transform(x[i:end_idxx])
+        corrections = []
+        for idx, instance in enumerate(results):
+            correct = y[idx].lower().split(' ')
+            corrected = instance.get_corrected_sentence().lower().split(' ')
+            wrong = x[idx].lower().split(' ')
+            for token in zip(correct, wrong, corrected, instance.is_error):
+                word_count = word_count + 1
+                if token[0] != token[1]:
+                    error_count = error_count + 1
+                    if token[0] == token[2]:
+                        correction_count = correction_count + 1
+                    if token[3]:
+                        detected_count = detected_count + 1
+                    corrections.append(token)
+        print('Batch number: '+str(i))
+        print(correction_count)
+        print(detected_count)
+        print(error_count)
+        print(word_count)
+        with open('results.csv', 'a') as out:
+            csv_out = csv.writer(out)
+            for row in corrections:
+                csv_out.writerow(row)
+    except:
+        with open('error.log', 'a') as myfile:
+            myfile.write('Error at batch: '+i+'\n')
+     
